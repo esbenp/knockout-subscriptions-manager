bf6a5f87faa833566589115c98b7b955fd8fdd02
Esben Petersen <esbenspetersen@gmail.com>
2015-03-29 15:26:33 +0200

    Typo

--- karma.conf.js
+++ karma.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
   };
 
   if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
+    settings.browsers = ['Chrome_travis_ci', 'Firefox'];
   }
 
   config.set(settings);
